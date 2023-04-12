14f8c8e79768f9f2cbe2ede80ea18aee70b4fc43
Wahyu <firmansyahw503@gmail.com>
2023-04-12 14:34:50 +0700

    membuat input public

--- oopkelas.cpp
+++ oopkelas.cpp
@@ -6,3 +6,16 @@ class Mahasiswa {
 private:
 	int nim;
 	string nama;
+public:
+	void inputdata() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan nama : ";
+		cin >> nama;
+	}
+	void tampildata() {
+		cout << "\nNIM : " << nim << endl;
+		cout << "nama : " << nama << endl;
+	}
+};
+
